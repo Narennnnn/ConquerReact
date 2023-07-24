1b16144e8ded11c0063b18ecbeb5b365a0cad5b3
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-24 23:50:39 +0530

    Update stateCounter.js

--- src/stateCounter.js
+++ src/stateCounter.js
@@ -8,11 +8,19 @@ function App() {
   const incrementCount = () => {
     setCount(count + 1); // Update the state with a new value
   };
+  const decrementCount = () => {
+    setCount(count - 1); // Update the state with a new value
+  };
+  const countSetZero = () => {
+    setCount(0); // Update the state with a new value
+  };
 
   return (
     <div>
       <h1>Count: {count}</h1>
       <button onClick={incrementCount}>Increment</button>
+      <button onClick={decrementCount}>Decrement</button>
+      <button onClick={countSetZero}>setToZero</button>
     </div>
   );
 }
