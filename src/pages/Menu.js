f48a8159a50594bec8d10ebb609a22f2795e5263
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-27 00:45:16 +0530

    ReactRouterDomPages.js

---
+++ src/pages/Menu.js
@@ -0,0 +1,17 @@
+export const Menu = () => {
+    return <p>
+        Appetizers:
+        Enchanted Forest Bruschetta
+        Starlight Shrimp Tempura
+        Unicorn Tears Soup
+        Main Courses:
+        Dragonfire Grilled Steak
+        Mermaid's Song Pasta
+        Phoenix Risotto
+        Vegetarian Delights:
+        Garden of Eden Salad
+        Fairy Ring Stir-Fry
+        Gaia's Blessing Bowl
+        Desserts:
+        10. Enchanted Chocolate Fondue</p>
+};
