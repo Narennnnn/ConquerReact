082af4d87b1529e850752d1bac39d1de76215680
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-30 01:22:52 +0530

    Create LoginFormApp.js

---
+++ src/LoginFormApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginFormusingUseForm';
+
+import './App.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
