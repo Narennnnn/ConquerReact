09bf0490dd4be4795bf48871b1549605a7cc4ba0
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-29 23:13:25 +0530

    Create appReactquery.js

---
+++ src/appReactquery.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState, useContext, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Profile } from './pages/profile'; // Use uppercase 'Profile' here
+import { Contact } from './pages/Contact';
+import { Navbar } from './navbar';
+import './App.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const AppContext = createContext(); // Update variable name here
+
+function App() {
+  const client = new QueryClient();
+  const [userName, setUserName] = useState("Harsh");
+  return (
+    <div className="App">
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ userName, setUserName }}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/profile' element={<Profile />} /> {/* Updated to lowercase 'profile' */}
+              <Route path='/contact' element={<Contact />} /> {/* Updated to lowercase 'contact' */}
+              <Route
+                path='*'
+                element={
+                  <div>
+                    <h1>Page not found</h1>
+                    <h2>Error 404</h2>
+                  </div>
+                }
+              />
+            </Routes>
+
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
+    </div>
+  );
+}
+
+export default App;
