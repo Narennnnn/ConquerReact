fc48778ad800f12011f902c01abadaaa850837db
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-30 01:22:21 +0530

    Create LoginFormUsingControlledComp.js

---
+++ src/LoginFormUsingControlledComp.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+const LoginForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Process the form data here (e.g., submit to server)
+        console.log(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default LoginForm;
